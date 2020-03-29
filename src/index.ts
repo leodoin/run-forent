79aed7e9b41f9494537bb2145831d7bb4bbf92ee
leodoin <leodoin@gmail.com>
2020-03-29 20:34:19 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ let app = express();
 // Setup server port
 var port = process.env.PORT || 8080;
 // Send message for default URL
-app.get('/', (req, res) => res.send('Hello World with Express'));
+app.get('/hello-world', (req, res) => res.send('Hello World with Express'));
 // Launch app to listen to specified port
 app.listen(port, function () {
      console.log("Running run-forest on port " + port);
