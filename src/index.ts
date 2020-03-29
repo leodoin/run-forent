23cfa337442f354649b20084bf602bf7dab63fad
leodoin <leodoin@gmail.com>
2020-03-29 20:13:10 +0200

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+// Initialize the app
+let app = express();
+// Setup server port
+var port = process.env.PORT || 8080;
+// Send message for default URL
+app.get('/', (req: any, res: { send: (arg0: string) => any; }) => res.send('Hello World with Express'));
+// Launch app to listen to specified port
+app.listen(port, function () {
+     console.log("Running run-forest on port " + port);
+});
